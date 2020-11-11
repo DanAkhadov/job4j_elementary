95acc8178c46297d381e813a5fb9c815f0b0d2d4
Даниял Ахадов <d.aha@bk.ru>
2020-11-11 16:01:13 +0400

    3. Арифметические операции. [#404926]

--- src/ru/job4j/Info.java
+++ src/ru/job4j/Info.java
@@ -3,5 +3,14 @@ package ru.job4j;
 public class Info {
     public static void main(String[] args) {
         System.out.println("11.10.2020");
+        int age = 18;
+        boolean sex = true;
+        float height = 1.75F;
+        short numberOfBricks = 18_010;
+        long numberOfBacteria = 1_000_999_999_000L;
+        long distanceToSpaceObjects = 1_000_000_000_000_000_000L;
+        boolean workPermit = true;
+        long fileSize = 1_000_000_000_000L;
+
     }
 }
