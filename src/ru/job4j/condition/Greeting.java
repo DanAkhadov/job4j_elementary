e0e3908fb1373f6c15658dafc0fe0c910b504801
Даниял Ахадов <d.aha@bk.ru>
2020-11-11 20:20:59 +0400

    9.2. Checkstyle [#404929]

--- src/ru/job4j/condition/Greeting.java
+++ src/ru/job4j/condition/Greeting.java
@@ -2,10 +2,6 @@ package ru.job4j.condition;
 
 public class Greeting {
     public static void main(String[] args) {
-        String idea = "I like Java!";
-        System.out.println(idea);
-        System.out.println(idea + " But I am a newbie.");
-        int year = 2020;
-        System.out.println(idea + " " + year);
+        System.out.println("Hello, World!");
     }
 }
