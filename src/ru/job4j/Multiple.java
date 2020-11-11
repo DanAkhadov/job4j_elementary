a19df3c38fe84c7f1ba34ce58863c6c1a32abd6b
Даниял Ахадов <d.aha@bk.ru>
2020-11-11 18:07:57 +0400

    6. Идеальный вес [#404913]

--- src/ru/job4j/Multiple.java
+++
@@ -1,9 +0,0 @@
-package ru.job4j;
-
-public class Multiple {
-    static public void main(String[] args) {
-        for (int i = 0; i <= 9; ++i) {
-            System.out.println("1 * " + i + " = " + i);
-        }
-    }
-}
