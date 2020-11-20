751612c3f2003468fbfb577b51d6085160f61bef
Даниял Ахадов <d.aha@bk.ru>
2020-11-20 20:35:25 +0400

    Tic-Tac-Toe

---
+++ src/com/max/TicTacToe.java
@@ -0,0 +1,114 @@
+package com.max;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Scanner;
+
+public class TicTacToe {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        String[] chars = {" ", " ", " ", " ", " ", " ", " ", " ", " "};
+        printCurrentState(chars);
+        String mySave = "";
+        int quantity = 0;
+        while (quantity < 9) {
+            quantity++;
+            userInput(scan, chars);
+            printCurrentState(chars);
+            mySave = winCheck(chars);
+            if (mySave.equals("X wins") || mySave.equals("O wins")) {
+                System.out.println(winCheck(chars));
+                System.exit(0);
+            }
+        }
+        System.out.println(winCheck(chars));
+    }
+
+    static void userInput(Scanner scan, String[] chars) {
+        String userMove = null;
+        for (boolean userInputCheckBool = false; !userInputCheckBool; userInputCheckBool = userInputCheck(userMove, chars))
+        {
+            System.out.print("Enter the coordinates: ");
+            userMove = scan.nextLine();
+        }
+
+    }
+
+    static boolean userInputCheck(String userMove, String[] chars) {
+        String[] userMoveArray = userMove.split(" ");
+
+        if (userMoveArray[0].matches("[0-9]") && userMoveArray[1].matches("[0-9]")) {
+            if (userMoveArray[0].matches("[1-3]") && userMoveArray[1].matches("[1-3]")) {
+                switch (userMove) {
+                    case "3 1":
+                        return charsOccupiedCheck(6, chars);
+                    case "2 1":
+                        return charsOccupiedCheck(3, chars);
+                    case "1 1":
+                        return charsOccupiedCheck(0, chars);
+                    case "3 2":
+                        return charsOccupiedCheck(7, chars);
+                    case "2 2":
+                        return charsOccupiedCheck(4, chars);
+                    case "1 2":
+                        return charsOccupiedCheck(1, chars);
+                    case "3 3":
+                        return charsOccupiedCheck(8, chars);
+                    case "2 3":
+                        return charsOccupiedCheck(5, chars);
+                    default:
+                        return charsOccupiedCheck(2, chars);
+                }
+            } else {
+                System.out.println("Coordinates should be from 1 to 3!");
+                return false;
+            }
+        } else {
+            System.out.println("You should enter numbers!");
+            return false;
+        }
+    }
+    public static int quantity = 0;
+    static boolean charsOccupiedCheck(int charNumber, String[] chars) {
+        if (chars[charNumber].matches("[XO]")) {
+            System.out.println("This cell is occupied! Choose another one!");
+            return false;
+        } else {
+            if (quantity % 2 == 1) {
+                chars[charNumber] = "O";
+            } else {
+                chars[charNumber] = "X";
+            }
+            quantity++;
+            return true;
+        }
+    }
+
+
+    static void printCurrentState(String[] chars) {
+        System.out.printf("---------%n| %s %s %s |%n| %s %s %s |%n| %s %s %s |%n---------%n", chars[0], chars[1], chars[2], chars[3], chars[4], chars[5], chars[6], chars[7], chars[8]);
+    }
+
+    static String winCheck(String[] chars) {
+        if (win(chars, "X") && win(chars, "O") || aLotMore(chars)) return "Impossible";
+        else if (win(chars, "X")) return "X wins";
+        else if (win(chars, "O")) return "O wins";
+        else if (Arrays.asList(chars).contains("_")) return "Game not finished";
+        else return "Draw";
+    }
+
+    static boolean aLotMore(String[] chars) {
+        return Math.abs((Collections.frequency(Arrays.asList(chars), "X") - Collections.frequency(Arrays.asList(chars), "O"))) > 1;
+    }
+
+    static boolean win(String[] chars, String player) {
+        return  (chars[0].equals(player) && chars[1].equals(player) && chars[2].equals(player)) ||
+                (chars[3].equals(player) && chars[4].equals(player) && chars[5].equals(player)) ||
+                (chars[6].equals(player) && chars[7].equals(player) && chars[8].equals(player)) ||
+                (chars[0].equals(player) && chars[3].equals(player) && chars[6].equals(player)) ||
+                (chars[1].equals(player) && chars[4].equals(player) && chars[7].equals(player)) ||
+                (chars[2].equals(player) && chars[5].equals(player) && chars[8].equals(player)) ||
+                (chars[0].equals(player) && chars[4].equals(player) && chars[8].equals(player)) ||
+                (chars[2].equals(player) && chars[4].equals(player) && chars[6].equals(player));
+    }
+}
